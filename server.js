66902694fc2d256ec11fca890ba43d095e4d494b
nilocy <mahaffec@gmail.com>
2015-10-01 16:49:29 -0400

    allows get requests to effect messages; currently functions to get movie plots based off movie titles

--- server.js
+++ server.js
@@ -3,31 +3,50 @@ var express    =  require('express'),
     http       =  require('http'),
     socketIo   =  require('socket.io'),
     bodyParser =  require('body-parser'),
-    morgan     =  require('morgan');
+    morgan     =  require('morgan'),
+    request =  require('request');
 
 var app = express();
 var wrapperServer=http.Server(app);
 var io = socketIo(wrapperServer);
 
+
+
 app.use(express.static(__dirname+'/client'));
 
-io.on('connection',function(socket){
+io.on('connection',function(socket, response){
   console.log('connected');
   socket.on('send message', function(message){
     console.log('Recieved:', message);
-    var chat = message;
-    // chat.save(function(){
-      io.emit('emit message', message)
-    // });
+
+    request('http://www.omdbapi.com/?t='+message.text+'&y=&plot=long&r=json', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        data=JSON.parse(body);
+        // console.log(data.Plot);
+        io.emit('emit message', {text: data.Plot})
+      }
+    });
+
+
   });
 });
 
 
-app.get('/',function(req,res){
-  res.sendFile(__dirname+'/client/index.html')
+
+app.get('/', function(req,res){
+
+
+  res.sendFile(__dirname+'/client/index.html');
+
+
+  console.log("get request");
+
 });
 
 var port=process.env.PORT || '8080';
+
+
+// app.listen(port, function(){
 wrapperServer.listen(port,function(){
     console.log('started...');
 });
